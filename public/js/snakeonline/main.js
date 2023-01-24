3c1037c57951bea69032e4767baebab476bea22f
jannik <j.pohlmann@outlook.de>
2023-01-24 16:48:02 +0100

    updated snake online client

--- public/js/snakeonline/main.js
+++ public/js/snakeonline/main.js
@@ -6,25 +6,127 @@ canvas.height=500;
 let ctx = canvas.getContext("2d");
 
 const PORT = 44444;
-// const IP = "127.0.0.1";
-const IP = "206.189.102.50";
 
-let username = document.getElementById("username");
-let startButton = document.getElementById("start");
-let restartButton = document.getElementById("restart");
-let startMenu = document.getElementById("startmenu");
-let deathmenu = document.getElementById("deathmenu");
-let deathCause = document.getElementById("deathCause");
+let usernameEle = document.getElementById("username");
+let startMenuEle = document.getElementById("startmenu");
+let deathMenuEle = document.getElementById("deathmenu");
+let connectionerrorEle = document.getElementById("connectionerror");
+let deathCauseEle = document.getElementById("deathCause");
+let leaderboardEle = document.getElementById("leaderboard");
+let leaderboardListEle = document.getElementById("lbList");
+let leaderboardSpanEle = leaderboardEle.querySelectorAll("button span")[1];
+let chatEle = document.getElementById("chat");
+let chatListEle = document.getElementById("chatList");
+let chatInputEle = document.getElementById("chatinput");
+let chatSpanEle = chatEle.querySelectorAll("button span")[1];
+
 
 let snakes = [];
 let apples = [];
 let obstacles = [];
 let safezones = [];
+let leaderboard = [];
+let chat = [];
 
 let selfSnake;
 let gridSize=10;
 let id;
 let initialized = false;
+let spawnCountdown = -1;
+let spawnCountDownTime = 3;
+
+let viewingleaderboard = false;
+let viewingchat = false;
+
+
+const playerSnakeColor="#33632b";
+const playerSnakeHeadColor=tColor(playerSnakeColor,-20);
+const snakeColor="#634c2b";
+const snakeHeadColor=tColor(snakeColor,-20);
+const playerGhostSnakeColor="#575e56";
+const playerGhostSnakeHeadColor=tColor(playerGhostSnakeColor,-20);
+const ghostSnakeColor="#7a7a7a";
+const ghostSnakeHeadColor=tColor(ghostSnakeColor,-20);
+const obstacleColor="#0a0a0f";
+const appleColor="#a03030";
+const safeZoneColor="#dce6ea";
+
+function tColor(col, amt) {
+    col = col.slice(1);
+    var num = parseInt(col, 16);
+    var r = (num >> 16) + amt;
+    var b = ((num >> 8) & 0x00FF) + amt;
+    var g = (num & 0x0000FF) + amt;
+    var newColor = g | (b << 8) | (r << 16);
+    return "#"+newColor.toString(16);
+}
+
+function toggleLeaderboardShow(){
+    if(leaderboardEle.getAttribute("open")=="true"){
+        leaderboardEle.setAttribute("open","false");
+        leaderboardSpanEle.style.transform = "rotate(0deg)";
+        viewingleaderboard=false;
+        leaderboardListEle.style.display="none";
+    }else{
+        leaderboardEle.setAttribute("open","true");
+        leaderboardSpanEle.style.transform = "rotate(90deg)";
+        viewingleaderboard=true;
+        leaderboardListEle.style.display="flex";
+        createLeaderBoard();
+    }
+}
+
+function toggleChatShow(){
+    if(chatEle.getAttribute("open")=="true"){
+        chatEle.setAttribute("open","false");
+        chatSpanEle.style.transform = "rotate(0deg)";
+        viewingchat=false;
+        chatListEle.style.display="none";
+        chatInputEle.parentElement.parentElement.style.display="none";
+    }else{
+        chatEle.setAttribute("open","true");
+        chatSpanEle.style.transform = "rotate(90deg)";
+        viewingchat=true;
+        chatListEle.style.display="flex";
+        chatInputEle.parentElement.parentElement.style.display="flex";
+        createChat();
+    }
+}
+
+function createChat(){
+    chatListEle.innerHTML="";
+    if(chat.length==0){
+        chatListEle.style.display="none"
+        return;
+    }else{
+        chatListEle.style.display="flex"
+    }
+    chat.forEach(e=>{
+        let div = document.createElement("div");
+        let usernamespan = document.createElement("span");
+        usernamespan.innerText=e.usr+ ": ";
+        let msgspan = document.createElement("span");
+        msgspan.innerText=e.msg;
+        div.appendChild(usernamespan);
+        div.appendChild(msgspan);
+        chatListEle.appendChild(div);
+    })
+    chatListEle.scrollTop=chatListEle.scrollHeight;
+}
+
+function createLeaderBoard(){
+    leaderboardListEle.innerHTML="";
+    leaderboard.forEach(e=>{
+        let div = document.createElement("div");
+        let usernamespan = document.createElement("span");
+        usernamespan.innerText=e.username;
+        let scorespan = document.createElement("span");
+        scorespan.innerText=e.score;
+        div.appendChild(usernamespan);
+        div.appendChild(scorespan);
+        leaderboardListEle.appendChild(div);
+    })
+}
 
 const Camera = {
     scale :10,
@@ -94,8 +196,21 @@ function sendMessage(type,object){
     ws.send(JSON.stringify({t:type,data:object}));
 }
 
+msgHandler.addMsgHandle("lb",data=>{
+    leaderboard=data;
+    if(viewingleaderboard){
+        createLeaderBoard();
+    }
+})
+
 msgHandler.addMsgHandle("msg",data=>{
-    console.log(data);
+    chat.push(data);
+    if(chat.length>30){
+        chat.splice(0,1);
+    }
+    if(viewingchat){
+        createChat();
+    }
 })
 
 msgHandler.addMsgHandle("init",data=>{
@@ -103,6 +218,7 @@ msgHandler.addMsgHandle("init",data=>{
     snakes=data.snakes;
     obstacles=data.obstacles;
     safezones=data.safezones;
+    spawnCountDownTime=data.spawnCountDownTime;
 
     gridSize=data.gridSize;
     id=data.id;
@@ -115,13 +231,14 @@ msgHandler.addMsgHandle("init",data=>{
 
 msgHandler.addMsgHandle("snakespawn",data=>{
     let snake = snakes.find(s=>s.id==data.id);
+    if(data.id==id){
+        selfSnake=data;
+        Camera.x=selfSnake.head.x*Camera.scale-(canvas.width/2);
+        Camera.y=selfSnake.head.y*Camera.scale-(canvas.width/2);
+    }
     if(snake==null){
-        if(data.id==id){
-            selfSnake=data;
-        }
         snakes.push(data);
     }else{
-
         snake.dir=         data.dir;
         snake.state=       data.state;
         snake.eaten=       data.eaten;
@@ -134,6 +251,11 @@ msgHandler.addMsgHandle("snakespawn",data=>{
 msgHandler.addMsgHandle("snakechange",data=>{
     data.forEach((s,i)=>{
         if(s==null||snakes[i]==null)return;
+        if(s.id==id){
+            selfSnake.dir=s.dir;
+            selfSnake.state=s.state;
+            selfSnake.eaten=s.eaten;
+        }
         snakes[i].dir=s.dir;
         snakes[i].state=s.state;
         snakes[i].eaten=s.eaten;
@@ -147,8 +269,8 @@ msgHandler.addMsgHandle("snakeremove",data=>{
 })
 
 msgHandler.addMsgHandle("death",data=>{
-    deathmenu.style.display="flex";
-    deathCause.innerText=data.cause;
+    deathMenuEle.style.display="flex";
+    deathCauseEle.innerText=data.cause;
 })
 
 msgHandler.addMsgHandle("applechange",data=>{
@@ -161,6 +283,21 @@ msgHandler.addMsgHandle("appleadd",data=>{
     data.forEach(a=>{
         apples.push(a);
     })
+})
+
+msgHandler.addMsgHandle("obstacleadd",data=>{
+    data.forEach(o=>{
+        obstacles.push(o);
+    })
+})
+
+msgHandler.addMsgHandle("obstacleremove",data=>{
+    let i = obstacles.findIndex(o=>o.id==data);
+    if(i==-1){
+        console.error("tried removing non existing obstacle:"+ data);
+        return;
+    }
+    obstacles.splice(i,1);
 })
 
 msgHandler.addMsgHandle("username",data=>{
@@ -221,6 +358,21 @@ function loop(){
 
 function update(){
     Camera.update();
+
+    if(selfSnake!=null&&selfSnake.state=="it"&&spawnCountdown==-1){
+        startSpawnCountDown();
+    }
+}
+
+async function startSpawnCountDown(){
+    for (let i = spawnCountDownTime; i > 0; i--) {
+        spawnCountdown=i;
+        await waitFor(1);
+    }
+    spawnCountdown=0;
+    await waitFor(1);
+    spawnCountdown=-1;
+
 }
 
 function renderBodies(body,altcolor){
@@ -233,62 +385,138 @@ function renderBodies(body,altcolor){
 function render(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
 
-    ctx.fillStyle="#b8dced";
-    safezones.forEach(s => {
-        Camera.transformDraw(s.x,s.y,s.size,s.size);
-    });
+    ctx.fillStyle=safeZoneColor;
+    if(selfSnake!=null&&(selfSnake.state=="i")){
+        safezones.forEach(s => {
+            Camera.transformDraw(s.x,s.y,s.size,s.size);
+        });
+    }
 
     snakes.forEach(snake => {
         if(snake.state=="d")return;
         if(snake.id==id){
-            ctx.fillStyle="#33632b";
-            renderBodies(snake.head,"#4b8c48");
+            if(snake.state=="i"||snake.state=="it"){
+                ctx.fillStyle=playerGhostSnakeHeadColor;
+                renderBodies(snake.head,playerGhostSnakeColor);
+
+                if(spawnCountdown!=-1){
+                    let transform = Camera.transform(snake.head.x,snake.head.y);
+                    ctx.fillText(spawnCountdown.toString(),transform.x-(ctx.measureText(spawnCountdown.toString()).width/2)+(Camera.scale/2),transform.y-5);
+                }
+            }else{
+                ctx.fillStyle=playerSnakeHeadColor;
+                renderBodies(snake.head,playerSnakeColor);
+            }
         }else{
-            ctx.fillStyle="#634c2b";
-            renderBodies(snake.head,"#8c7048");
+            if(snake.state=="i"||snake.state=="it"){
+                ctx.fillStyle=ghostSnakeHeadColor;
+                renderBodies(snake.head,ghostSnakeColor);
+            }else{
+                ctx.fillStyle=snakeHeadColor;
+                renderBodies(snake.head,snakeColor);
+            }
+            ctx.fillStyle=snakeHeadColor;
+            renderBodies(snake.head,snakeColor);
             let transform = Camera.transform(snake.head.x,snake.head.y);
             ctx.fillText(snake.username,transform.x-(ctx.measureText(snake.username).width/2)+(Camera.scale/2),transform.y-5);
         }
     });
 
-    ctx.fillStyle="#a03030";
+    ctx.fillStyle=appleColor;
     apples.forEach(apple => {
-        Camera.transformDraw(apple.x,apple.y);
+        let transform = Camera.transform(apple.x,apple.y);
+        ctx.beginPath();
+        ctx.arc(transform.x+transform.w/2,transform.y+transform.w/2,transform.w/2,0,Math.PI*2);
+        ctx.fill();
     });
 
-    ctx.fillStyle="#0a0a0f";
+    ctx.fillStyle=obstacleColor;
     obstacles.forEach(obstacle => {
         Camera.transformDraw(obstacle.x,obstacle.y);
     });
 
+    if(selfSnake==null||selfSnake.state!="i"){
+        ctx.fillStyle=obstacleColor;
+        ctx.globalAlpha = 0.8;
+        safezones.forEach(s => {
+            Camera.transformDraw(s.x,s.y,s.size,s.size);
+        });
+        ctx.globalAlpha = 1;
+    }
+
     {
         let transform = Camera.transform(0,0,gridSize,gridSize);    
-        ctx.strokeStyle="#0a0a0f";
+        ctx.strokeStyle=obstacleColor;
         ctx.lineWidth = 2;
         ctx.strokeRect(transform.x,transform.y,transform.w,transform.h);
         ctx.lineWidth = 1;
     }
 }
 
+async function waitFor(seconds){
+    return new Promise((res)=>{
+        setTimeout(()=>{
+            res();
+        },seconds*1000);
+    });
+}
 
-startButton.onclick=()=>{
-    if(username.value.trim().length===0){
-        username.value="";
+function remainingCheck(self,remainingMax){
+    let remaining =(remainingMax-self.value.length);
+    if(remaining<0){
+        self.value=self.value.slice(0,remainingMax);
+        return;
+    }
+    self.parentElement.setAttribute("remaining",remaining.toString());
+}
+
+usernameEle.addEventListener("keydown",e=>{
+    if(e.key=="Enter"){
+        startClick();
+    }
+})
+
+chatInputEle.addEventListener("click",e=>{
+    e.stopPropagation();
+})
+
+
+chatInputEle.addEventListener("keydown",e=>{
+    e.stopPropagation();
+
+    if(e.key=="Enter"){
+        if(chatInputEle.value.trim().length!=0){
+            sendMessage("msg",chatInputEle.value);
+            chatInputEle.parentElement.setAttribute("remaining",32);
+            chatInputEle.value="";
+        }
+    }
+})
+
+function startClick(){
+    if(usernameEle.value.trim().length===0){
+        usernameEle.value="";
+        alert("please enter a username");
         return;
     };
-    startButton.onclick=null;
-    startMenu.style.display="none";
+    startMenuEle.style.display="none";
     ws = new WebSocket("wss://jannik323.software:"+PORT);
-
-    setInterval(loop,16);
+    // ws = new WebSocket("ws://127.0.0.1:"+PORT);
 
     ws.onerror=()=>{
-        alert("Unable to connect to host.\nTry again later!");
-        location.reload(1);
+        connectionerrorEle.style.display = "block";
+        leaderboardEle.style.display="none";
+        chatEle.style.display="none";
+    }
+
+    ws.onopen=()=>{
+        setInterval(loop,16);
+        leaderboardEle.style.display="flex";
+        chatEle.style.display="flex";
     }
 
     ws.addEventListener("open", () =>{
-        sendMessage("username",username.value);
+        sendMessage("username",usernameEle.value);
     });
     
     ws.addEventListener('message', function (event) {
@@ -302,21 +530,20 @@ startButton.onclick=()=>{
             if(selfSnake==null||selfSnake.state=="d")return;
             switch(e.key){
                 case "w":
-                    if(selfSnake.dir.y==1&&selfSnake.length>0)break;
-                    sendMessage("move","up");
+                    sendMessage("input","up");
                 break;
                 case "s":
-                    if(selfSnake.dir.y==-1&&selfSnake.length>0)break;
-                    sendMessage("move","down");
+                    sendMessage("input","down");
                 break;
                 case "a":
-                    if(selfSnake.dir.x==1&&selfSnake.length>0)break;
-                    sendMessage("move","left");
+                    sendMessage("input","left");
                 break;
                 case "d":
-                    if(selfSnake.dir.x==-1&&selfSnake.length>0)break;
-                    sendMessage("move","right");
+                    sendMessage("input","right");
                 break;
+                // case " ":
+                //     sendMessage("input","shoot");
+                // break;
             }
         }
     });
@@ -325,7 +552,11 @@ startButton.onclick=()=>{
     });
 }
 
-restartButton.onclick=()=>{
-    sendMessage("restart",{});
+function restartClick(){
+    sendMessage("restart",null);
     deathmenu.style.display="none";
+}
+
+function reloadClick(){
+    location.reload(1);
 }
